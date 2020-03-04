1765c9050c77bb5bae5c50e859a2bee6622e1192
Natalia Wit <natwit95@gmail.com>
2020-03-04 17:55:03 -0500

    Wednesday Day 4

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ let mainDiv = document.querySelector(".main-div")
 let wordCard = mainDiv.querySelector(".word-card")
 let formBox = document.createElement("div")
 let rhymesBox = document.createElement("div")
+let userForm = document.querySelector(".user-form")
+let userDiv = document.querySelector(".user-div")
+let gameOverDiv = document.querySelector(".game-over")
+let currentUser = " "
+let timerH2 = document.querySelector(".timer")
+let startButton = document.querySelector(".play")
+let rhymesForm = document.querySelector(".rhymes-form")
+let scoreCard = document.querySelector(".current-score")
+let finalScore = document.querySelector(".final-score")
+let currentScore = 0
+let rightAnswers = []
+let wordsArr = []
+const wrong = new Audio("./sounds/wrong.mp3")
+const right = new Audio("./sounds/right.mp3")
 
-
-        
-
+function startGame() {
     fetch("http://localhost:3000/words/")
     .then((response) => {
         return response.json();
@@ -21,10 +33,13 @@ let rhymesBox = document.createElement("div")
             createWordBox(data)
             createFormBox()
             createRhymesBox()
-          
+            let rhymesForm = document.querySelector(".rhymes-form")
+            rhymesForm.style.visibility = "hidden"
+            currentScore = 0
+            scoreCard.innerText = `Score:${currentScore}`
         })
  })//.THEN
-    
+}//START GAME()
     
 
 
@@ -41,7 +56,7 @@ function populateRandomWord(data){
             let wordBox = document.createElement("div")
             wordBox.className = "word-box"
             wordBox.innerHTML = `<h1 class = "game-word">${data.word}</h1>`
-            wordCard.append(wordBox)
+            wordCard.innerHTML = `${wordBox.innerHTML}`
         }
         
     function createFormBox(){
@@ -64,27 +79,38 @@ function populateRandomWord(data){
         rhymesBox.innerHTML = `<ul class= "rhymes-list"></ul>`
         formBox.append(rhymesBox)
         let rhymesForm = document.querySelector(".rhymes-form")
-        //    console.log(rhymesForm) 
         }
 
-        document.addEventListener("submit", e => {
+        mainDiv.addEventListener("submit", e => {
         e.preventDefault()
         
         let gameWord = document.querySelector(".game-word").innerText
         let form = document.querySelector(".rhymes-form")
         let ul = document.querySelector(".rhymes-list")
+        
        
-        console.log(form)
         let input = document.querySelector("#rhyme").value
         
         fetch(`https://api.datamuse.com/words?rel_rhy=${gameWord}`)
         .then(resp => resp.json())
         .then(resp => {
-            let wordsArr = resp.map(word => word.word)
+            wordsArr = resp.map(word => word.word)
             
-            if (wordsArr.includes(input)){
+            if (wordsArr.includes(input) && !rightAnswers.includes(input)){
+            right.play()
             ul.innerHTML += `<li class= "each-word">${input}</li>` 
+            rightAnswers.push(input)
+            currentScore++ 
+            scoreCard.innerText = `Score:${currentScore}`
             }//IF STATEMENT
+            else if(rightAnswers.includes(input)){
+                console.log("STOP CHEATING")
+            } 
+            else if(!wordsArr.includes(input)){
+                wrong.play()
+                console.log("That shit dont rhyme son")
+
+            }
             
             form.reset()
             })//.then
@@ -93,7 +119,9 @@ function populateRandomWord(data){
          document.addEventListener("click", e => {
              switch(e.target.className) {
            case "play":
-            console.log(e.target)
+            let rhymesForm = document.querySelector(".rhymes-form")
+            e.target.style.visibility = "hidden"
+            rhymesForm.style.visibility = "visible"
                 timerFunc()
                 break;
                 default:
@@ -102,7 +130,6 @@ function populateRandomWord(data){
   })//CLICK EVENTS 
 
          function timerFunc() {
-            let timerH2 = document.querySelector(".timer")
             let clear = setInterval(decrimentTimer,1000)
 
             function decrimentTimer() {
@@ -111,12 +138,71 @@ function populateRandomWord(data){
                 timerH2.innerHTML = newTime
                     if (newTime <= 0){
                         clearInterval(clear)
-                        let form = document.querySelector(".rhymes-form")
-                        form.style.display = "none"
+                        // let form = document.querySelector(".rhymes-form")
+                        // form.style.display = "none"
+                        finalScore.innerText = `YOUR SCORE:${currentScore}`
+                        gameOverDiv.style.visibility = "visible"
+                        createGameInstance()
+
                     }//IF STATEMENT
             }//Decrament
         }//timerFunc
 
+
+    userForm.addEventListener("submit", e => {
+        if (userForm.name.value && userForm.username.value) {
+            e.preventDefault()
+            nameInput = userForm.name.value
+            usernameInput = userForm.username.value
+            data = {name: nameInput,
+            username: usernameInput
+            }
+
+            fetch("http://localhost:3000/users",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accepts: "application/json"
+                },
+                body: JSON.stringify(data)
+            })//FETCH
+            .then(response => response.json())
+            .then(user => {
+                currentUser = user.id
+                userDiv.style.visibility = "hidden"
+                startGame()
+            })//.THEN
+        }else{
+            alert("You must enter both a name and username")
+        }//IF ELSE
+    })//USERNAME SUBMIT LISTENER 
+
+    gameOverDiv.addEventListener("click", e => {
+        let playAgainbutton = document.querySelector(".play-again")
+        if(e.target.className === "play-again"){
+            gameOverDiv.style.visibility = "hidden"
+            startButton.style.visibility = "visible"
+            currentScore = 0
+            scoreCard.innerText = `Score:${currentScore}` 
+            startGame()
+            timerH2.innerHTML = 60
+        }
+        })//GAME OVER LISTENER
+        
+        function createGameInstance() {
+            let data = {score: currentScore,
+            user_id: currentUser
+            } 
+            fetch("http://localhost:3000/games", {
+            method: "POST",
+            headers: {"Content-Type": "application/json",
+            Accept: "application/json"  
+            },
+            body:JSON.stringify(data)
+            })//FETCH
+            .then(resp => resp.json())
+            .then()
+        }//CREATE GAME()
 });//dom
 
 
